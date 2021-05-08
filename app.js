aa165558d7deedc240778a8fbfa04ea086e66f3b
ex.codes <kendinikertenkelebek@me.com>
2021-05-08 17:04:42 +0300

    Cray - Eat Your Heart Out.mp3

--- app.js
+++
@@ -1,8 +0,0 @@
-const student = {
-  name: "Elif",
-  age: 20,
-  location: "Antalya",
-  hobbies: ["music", "reading", "chess"],
-};
-
-console.log(student.name);
---
+++ example.config.js
@@ -0,0 +1,8 @@
+'use strict';
+
+// NOTE: Change the file name to '.config.js'
+
+/* Insert Client ID */
+exports.ID = '';
+/* Insert Client Secret */
+exports.SECRET = '';
